8f532938a82e82a496a526802d8ba985d6c00098
Majid Parvin <parvin.majid@hedcoint.com>
2018-10-07 16:38:02 +0330

    align layouts

---
+++ src/components/Layouts/Row/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="row">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+const Row = {
+  name: "Row"
+}
+export default Row
+
+if (typeof window !== "undefined" && window.Vue) {
+  window.Vue.component(Row.name, Row)
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .row {
+    display: flex;
+    flex-wrap: wrap;
+  }
+</style>
