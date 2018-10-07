09d041a91430ff2925a52fdc5b0249a5a09fea59
Majid Parvin <parvin.majid@hedcoint.com>
2018-10-07 21:14:44 +0330

    align eslintrc file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,44 +1,47 @@
 // https://eslint.org/docs/user-guide/configuring
 
 module.exports = {
-	root: true,
-	parserOptions: {
-		parser: 'babel-eslint'
-	},
-	env: {
-		browser: true
-	},
-	extends: [
-		// https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
-		// consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-		'plugin:vue/essential',
-		// https://github.com/standard/standard/blob/master/docs/RULES-en.md
-		'standard'
-	],
-	// required to lint *.vue files
-	plugins: ['vue'],
-	// add your custom rules here
-	rules: {
-		'key-spacing': [ 'error', {
-			'singleLine': {
-				'beforeColon' : false,
-				'afterColon'  : true
-			},
-			"align": {
-				"beforeColon" : true,
-				"afterColon"  : true,
-				"on"          : "colon"
-			}
-		}],
-		// allow async-await
-		'generator-star-spacing': 'off',
-		// allow using new
-		'no-new': 'off',
-		// allow trailing spaces
-		'no-trailing-spaces': 'off',
-		'space-before-function-paren': 'off',
-		// allow debugger during development
-		'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-		"quotes": "off"
-	}
-};
+  root: true,
+  parserOptions: {
+    parser: "babel-eslint"
+  },
+  env: {
+    browser: true
+  },
+  extends: [
+    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+    "plugin:vue/essential",
+    // https://github.com/standard/standard/blob/master/docs/RULES-en.md
+    "standard"
+  ],
+  // required to lint *.vue files
+  plugins: ["vue"],
+  // add your custom rules here
+  rules: {
+    "key-spacing": [
+      "error",
+      {
+        singleLine: {
+          beforeColon: false,
+          afterColon: true
+        },
+        align: {
+          beforeColon: true,
+          afterColon: true,
+          on: "colon"
+        }
+      }
+    ],
+    // allow async-await
+    "generator-star-spacing": "off",
+    // allow using new
+    "no-new": "off",
+    // allow trailing spaces
+    "no-trailing-spaces": "off",
+    "space-before-function-paren": "off",
+    // allow debugger during development
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    quotes: "off"
+  }
+}
