620bdfbb9d94e4d4b6dd6ba8896eabd011913f29
Majid Parvin <parvin.majid@hedcoint.com>
2018-10-07 16:51:00 +0330

    add gallery reducers

--- src/reducers/galleryReducer.js
+++ src/reducers/galleryReducer.js
@@ -1,33 +1,30 @@
-import { handleActions } from '../utils'
-import { ROOT_STATE } from '../store'
-import galleryActionType from '../actions/galleryAction/galleryActionType'
+import { handleActions } from "../utils"
+import { ROOT_STATE } from "../store"
+import galleryActionType from "../actions/galleryAction/galleryActionType"
 const { GET_PHOTOS } = galleryActionType
 
 // reducer for gallery actions
 const reducers = {
   [GET_PHOTOS]: {
-    PENDING: (state) => {
+    PENDING: state => {
       return {
         ...state,
-        loading: true,
-        error: undefined
+        loading : true,
+        error   : undefined
       }
     },
     FULFILLED: (state, action) => {
       return {
         ...state,
-        photos: [
-          ...state.photos,
-          ...action.payload
-        ],
-        loading: false
+        photos  : [...state.photos, ...action.payload],
+        loading : false
       }
     },
     REJECTED: (state, action) => {
       return {
         ...state,
-        error: action.payload,
-        loading: false
+        error   : action.payload,
+        loading : false
       }
     }
   }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,5 @@
-import { combineReducers } from 'redux'
-import gallery from './galleryReducer'
+import { combineReducers } from "redux"
+import gallery from "./galleryReducer"
 
 export default combineReducers({
   gallery
